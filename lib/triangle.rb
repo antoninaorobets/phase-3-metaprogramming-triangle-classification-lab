68c4bee97ffd9b653f8bceb8cfb9d1266a4f0ecd
antoninaorobets <pani.antonina.don@gmail.com>
2022-04-24 22:16:13 -0400

    commit tree 6f58357cb766e54a27ae56d6698bdb71a2b3b1aa

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
+require "pry"
 class Triangle
-  # write code here
+
+  def initialize (a,b,c)
+    if (a > 0 and b > 0 and c > 0) and (a+b>c and a+c > b and b+c>a)
+        @a = a 
+        @b = b
+        @c = c
+      else
+        raise TriangleError
+    end
+  end
+
+  def kind
+    if (@a==@b and @b==@c and @a==@c)
+      return :equilateral
+    elsif (@a==@b or @b==@c or @a==@c)
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
